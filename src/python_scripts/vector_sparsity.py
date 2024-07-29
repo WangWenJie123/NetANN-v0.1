b91f9d781590438b594c737d760d17b577b5ebe4
Wenjiew <18117111969@163.com>
2024-07-29 14:18:36 +0800

    start add  experiments 2024/7/29

--- src/python_scripts/vector_sparsity.py
+++ src/python_scripts/vector_sparsity.py
@@ -23,10 +23,11 @@ def read_fvecs(file_name):
 base_file_name = '/home/wwj/Vector_DB_Acceleration/SPTAG/wwj_test/remote_nvme_vector_datasets/sift1M/sift_base.fvecs'
 query_file_name = '/home/wwj/Vector_DB_Acceleration/SPTAG/wwj_test/remote_nvme_vector_datasets/sift1M/sift_query.fvecs'
 
-xb_base = read_fvecs(query_file_name)
+xb_base = read_fvecs(base_file_name)
 
 d = 128
 xb_num = int(xb_base.size / d)
+sparse_vectors_num = 0
 
 vec_sparsity = 0
 for i in range(xb_num):
@@ -35,5 +36,8 @@ for i in range(xb_num):
             vec_sparsity += 1
     vec_sparsity = vec_sparsity / d * 100
     if vec_sparsity > 50.0:
-        print("vec[{index}] sparsity: {sparsity}%".format(index = i, sparsity = vec_sparsity))
+        sparse_vectors_num += 1
+        # print("vec[{index}] sparsity: {sparsity}%".format(index = i, sparsity = vec_sparsity))
     vec_sparsity = 0
+
+print("sparse vectors in database: ", sparse_vectors_num / xb_num * 100, "%")
